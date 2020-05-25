904d8cdd0833f5587c535c7f8b07969876ed2723
Ndizera Eddy <eddyndizera@gmail.com>
2020-05-25 09:56:53 +0200

    OA-12 refactor method find_before_mandate_state

--- models/review.py
+++ models/review.py
@@ -81,10 +81,10 @@ def find_before_mandate_state(mandate, current_roles):
         next((role for role in reviewers_order_list if role in current_roles), None)
     )
     filter_clause = [
-        Q(reviewer__role__contains=role) for role in roles_list_accessible_for_current_rev if role is not None
+        Q(reviewer__role__contains=role) for role in roles_list_accessible_for_current_rev if role
     ]
 
-    filter_clause_q = functools.reduce(lambda a, b: a | b, filter_clause, models.Q(reviewer__role=None))
+    filter_clause_q = functools.reduce(lambda a, b: a | b, filter_clause, Q(reviewer__role=None))
     qs = Review.objects.filter(mandate=mandate).filter(
         filter_clause_q
     ).filter(status=review_status.DONE)
