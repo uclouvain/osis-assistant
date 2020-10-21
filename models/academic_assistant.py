e22e5800c35103024a0be7be6ac79811e74a8e1b
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2020-10-21 12:28:12 +0200

    OA-14 : Remove one field and rename other

--- models/academic_assistant.py
+++ models/academic_assistant.py
@@ -46,7 +46,6 @@ class AcademicAssistant(models.Model):
     remark = models.TextField(null=True, blank=True)
     inscription = models.CharField(max_length=12, choices=assistant_phd_inscription.PHD_INSCRIPTION_CHOICES,
                                    null=True, default=None)
-    programmed_confirmation_test_date = models.DateField(null=True, blank=True)
     succeed_confirmation_test_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
